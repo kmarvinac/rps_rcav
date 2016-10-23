7442c91cda5a6a537fc6d2499c8f629683cce142
Kevin Marvinac <kevinmarvinac@users.noreply.github.com>
2016-10-23 17:37:31 -0500

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
+
 Rails.application.routes.draw do
+
+  get("/rock", :controller => "game", :action => "user_plays_rock")
+
+  get("/scissors", :controller => "game", :action => "user_plays_scissors")
+
+  get("/paper", :controller => "game", :action => "user_plays_paper")
+
+  root "game#user_plays_rock"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
